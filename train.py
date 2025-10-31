507bb81cb58c09fd37db217dc3f47fccb2326224
kannan-dedsec <kannansanchez@gmail.com>
2025-10-31 21:00:18 +0530

    Your message

--- train.py
+++ train.py
@@ -4,17 +4,22 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 import joblib
 import os
- 
+import json  
+
 
 os.makedirs('model', exist_ok=True)
+
+with open('params.json', 'r') as f:
+    params = json.load(f)
 
 df = pd.read_csv('data/data.csv')
 
 X = df.drop('species', axis=1)
 y = df['species']
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = LogisticRegression(max_iter=200)
+model = LogisticRegression(**params)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
@@ -27,5 +32,4 @@ with open('metrics.txt', 'w') as f:
 
 print(f"Model trained with accuracy: {accuracy}")
 print("Model saved to model/model.joblib")
-print("Metrics saved Metrics.txt")
-print("hellp")
+print("Metrics saved to metrics.txt")
